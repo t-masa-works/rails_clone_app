c98450575f0012c687711aeeb62b3979d3ca3253
tobe masa <t.masa.works@gmail.com>
2023-07-18 12:57:02 +0900

    Viewの作成、パスが通っているか表示確認

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,19 +1,35 @@
 class PostsController < ApplicationController
-
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
   def index
     @posts = Post.all
   end
 
+  def new
+    @post = Post.new
+  end
+
   def create
+    @post = Post.create(post_params)
+    @post.save
   end
 
   def edit
   end
 
   def destroy
+    @post.destroy
   end
 
   def show
   end
 
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
 end

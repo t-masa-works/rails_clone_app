443cd155ed06ef4d4df34033b715e5e534f31124
tobe masa <t.masa.works@gmail.com>
2023-07-18 11:15:55 +0900

    コントローラー仮作成、表示確認

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,19 @@
+class PostsController < ApplicationController
+
+  def index
+    @post = Post.all
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  def show
+  end
+
+end
